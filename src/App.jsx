4a9ee24da5ede148ef5cd6f633ffb6421b388f56
John Doe <johndoe@example.com>
2024-09-06 11:09:31 -0500

    modelo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import Login from './pages/login/Login'
 import Logo from './assets/Logo.png'
-
+import React from 'react'
 function App() {
 
   return (
--- src/pages/quiz/Quiz.jsx
+++ src/pages/quiz/Quiz.jsx
@@ -1,6 +1,10 @@
 import { useCallback } from "react";
 import useQuizStore from "../../stores/use-quiz-store";
 import "./Quiz.css";
+import { Canvas } from "react-three-fiber";
+import { Fseno } from "../../components/Fseno/Fseno";
+import React, { Suspense } from "react";
+import { OrbitControls } from "@react-three/drei"; 
 
 const Quiz = () => {
   const {quiz, incrementQuizProgress } = useQuizStore();
@@ -11,10 +15,26 @@ const Quiz = () => {
 
   return (
     <div className="quiz-container">
-      <h1 className="quiz-header">Hola</h1>
-      <p className="quiz-progress">Progreso del Quiz: {quiz.percentageQuizCompleted}%</p>
-      <button onClick={onHandleButtonNext}>Siguiente</button>
+
+      
+
+<div style={{width: "100%" , height: "10vh" }} />
+      <Canvas camera={{ zoom: 10, position: [15, 20, 15] }}>
+        <ambientLight intensity={0.5}/>
+        <pointLight position={[35, 35, 0]} intensity={0.4} />
+        <pointLight position={[-35, 35, 0]} intensity={0.4} />
+        <Suspense fallback={null}>
+        <Fseno/>
+        </Suspense>
+        <OrbitControls/>
+      </Canvas>
+
+
+
     </div>
+
+
+
   );
 };
 
