8c295180d7840e87c48f2d55de371722d4957a83
Priscilla-03 <priscilla.alvarado@correounivalle.edu.co>
2024-10-07 18:21:25 -0500

    Cambios de pagina de inicio

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -4,11 +4,12 @@ import { useCallback, useEffect } from "react";
 import useAuthStore from "../../stores/use-auth-store";
 import UserDAO from "../../daos/UserDAO";
 import { useNavigate } from "react-router-dom";
-import user_icon from "../../assets/person.png";
-import email_icon from "../../assets/email.png";
-import password_icon from "../../assets/password.png";
 
 const Login = () => {
+
+  const handleinvite = () =>{
+    navigate("/Quiz")
+  };
 
   const [action, setAction] = useState("Login");
 
@@ -47,41 +48,15 @@ const Login = () => {
 
   return (
     <div className="container">
-      <div className="header">
-        <div className="text">{action}</div>
+      <div className="header"> 
+        <h1>Bienvenidos a PLANET ENERGY</h1>
       </div>
-      <div className="inputs">
-        {action === "Login" ? <div></div> : <div className="input">
-          <img src={user_icon} alt="" />
-          <input type="text" placeholder=' Name' />
-        </div>}
-
-        <div className="input">
-          <img src={email_icon} alt="" />
-          <input type="email" placeholder=' Email ' />
-        </div>
-        <div className="input">
-          <img src={password_icon} alt="" />
-          <input type="password" placeholder=' Password ' />
-        </div>
+      <div className="button-container">
+        <button className={action === "Sing Up" ? "button gray" : "button"} onClick={() => { handleLogin("Login") }}>Login</button>
+        <button onClick={handleinvite}>invitado</button>
       </div>
-      <div className="forgot-password">Lost Password?<span>Click Here!</span></div>
-      {user ? (
-        <>
-          <p className="welcome-text">Bienvenido, {user.displayName}</p>
-          <button className="button-logout" onClick={handleLogout}>
-            Cerrar sesión
-          </button>
-        </>
-      ) : (
-        <div className="button-container">
-          <button className={action === "Login" ? "button gray" : "button"} onClick={() => { setAction("Sing Up") }}>Sing Up</button>
-          <button className={action === "Sing Up" ? "button gray" : "button"} onClick={() => { handleLogin("Login") }}>Login</button>
-        </div>
-      )}
     </div>
   );
 };
 
 export default Login;
-
