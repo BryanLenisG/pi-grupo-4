885544cef9c3da101b3b43371071b290f6e34558
BryanLenisG <bryan.lenis@correounivalle.edu.co>
2024-10-21 19:04:16 -0500

    Introduccion Erosion del suelo

--- src/pages/erosion/Erosion.jsx
+++ src/pages/erosion/Erosion.jsx
@@ -1,7 +1,17 @@
+import './Erosion.css';
+
+
 
 const Erosion = () => {
-    return <div>Página de Erosion</div>;
-  };
-  
-  export default Erosion;
-  
+
+  return (
+    <div className="erosion-container">
+      <p className="erosion-text">
+        La erosión del suelo es un proceso natural que ocurre cuando la capa superior de la tierra es arrastrada o desgastada por factores como el agua, el viento o la actividad humana.
+      </p>
+    </div>
+    
+  );
+};
+
+export default Erosion;
