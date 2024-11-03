66525b3da1670f627503d06dc675939ff79eee7f
BryanLenisG <bryan.lenis@correounivalle.edu.co>
2024-11-03 16:39:51 -0500

    Cambios en el modelo 3D y texturas

--- src/pages/erosion/Erosion.jsx
+++ src/pages/erosion/Erosion.jsx
@@ -1,12 +1,39 @@
 import './Erosion.css'; 
+import { Canvas } from "react-three-fiber";
+import { useNavigate } from 'react-router-dom';
+import Ant from "../../components/model-3d/ant";
+import Earth from "../../components/model-3d/EarthErosion"
+import { OrbitControls } from "@react-three/drei";
 
 const Erosion = () => {
+  const navigate = useNavigate();
+  const handleBack = () => {
+    navigate(-1); 
+};
   return (
-    <div className="erosion-container">
+    <div>
+      <div className="earth-container">
+        <Canvas camera={{ position: [40, 10, 10], zoom: 8 }}
+          style={{ width: "130vw", height: "80vh", position: "absolute" }} >
+
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[10, 10, 10]} intensity={7} />
+          <OrbitControls autoRotate={true} />
+          <Earth/>
+        </Canvas>
+ 
+      </div>
+
+      <div className="erosion-container">
       <p className="erosion-text">
         La erosión del suelo es un proceso natural que ocurre cuando la capa superior de la tierra es arrastrada o desgastada por factores como el agua, el viento o la actividad humana.
       </p>
+      <button className="button-back" onClick={handleBack}>
+          Volver
+      </button>
     </div>
+    </div>
+
   );
 };
 
